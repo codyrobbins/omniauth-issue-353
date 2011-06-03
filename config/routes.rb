736fb437b5a0bc476101deadcf77313078b1ce6f
Cody Robbins <cody@sakuzaku.com>
2011-06-03 12:18:38 -0400

    Added some routes.

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,7 @@ OmniauthIssue353::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root(:to => 'dummy#set_session')
+  get('auth/facebook/callback' => 'dummy#show_session')
 end

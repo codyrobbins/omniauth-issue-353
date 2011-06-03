2a1a0616a8e9a9fc110305540ef1d826946b46f6
Cody Robbins <cody@sakuzaku.com>
2011-06-03 12:02:52 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthIssue353::Application.config.secret_token = '13643ae9dcbc04d8f30a3f684dc0e3a7b324a69e4b4aad98392cff6a32927b9b24b3b052e79b6451de3c29f8b81c6ae32a4ae53af455e3716c7f92665037bca2'
